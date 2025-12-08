a5ee26ca41f47fca02654a93f70a34ea02c8a9cc
hexch <hexch.c@gmail.com>
2025-12-08 23:41:49 +0900

    feat: Add Swift Package CI workflow and `UIApplicationDelegate` extension for default promo notification scheduling.

--- Sources/NotiKit/NotiKit.swift
+++ Sources/NotiKit/NotiKit.swift
@@ -11,3 +11,12 @@ public extension View {
     }
   }
 }
+
+public extension UIApplicationDelegate {
+  @available(iOS 15.0, *)
+  func prepareDefaultPromoNotifications() {
+    Task {
+      await NotiService.shared.schedule(defaultPromoModels)
+    }
+  }
+}
